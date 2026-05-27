7b60ee980c16f0f44c87d19777bcdf2f626c4578
vansh-Kumar7 <rkvansh76@gmail.com>
2026-05-27 20:59:20 +0530

    fixed production server url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = import.meta.env.VITE_API_URL
 
 function App() {
 
