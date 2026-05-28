661658079398e23a5c6d47c70f6a213b73ae074c
vansh-Kumar7 <rkvansh76@gmail.com>
2026-05-28 12:36:26 +0530

    final cookie auth fix

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -17,6 +17,7 @@ export const googleAuth = async (req,res) => {
             httpOnly:true,
             secure:true,
             sameSite:"None",
+            credentials: true,
             maxAge:7 * 24 * 60 * 60 * 1000
         })
 
